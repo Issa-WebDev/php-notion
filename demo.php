f0cbaccd01970f646b4297b79232aa752ba2a15c
Issa-WebDev <kissadev10@gmail.com>
2025-03-18 04:27:26 +0100

    php

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php 
+$name = "Kone";
+$nickname = "issa";
+$note1 = 10;
+$note2 = 15;
+$moyenne = ($note1 + $note2) / 2;
+echo "$name $nickname vous avez eu $moyenne moyenne";
+
+
+
---
+++ tableaux.php
@@ -0,0 +1,9 @@
+<?php
+// $notes = ["kone", "issa", [10, 20, 15]];
+
+$students = [
+	"name" => "Issa",
+	"notes" => [10, 12,18]
+];
+$students["name"] = "kisscoder";
+echo $students["name"];
